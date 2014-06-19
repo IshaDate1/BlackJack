377818bd5fbbb771c59e473586d917b40eb42d46
George Troulis <georgetroulis@gmail.com>
2014-06-19 04:42:25 +0300

    Replaced Player with DealerPlayer for the dealer in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,14 +3,16 @@ package main;
 //import cardgame.Card;
 import cardgame.Deck;
 import blackjack.Player;
+import blackjack.DealerPlayer;
 
-public class Main {
+public class Main
+{
 
 	public static void main(String[] args) {
 		Deck deck = new Deck(true);
-		
+
 		Player player = new Player("George", deck.draw(), deck.draw());
-		Player dealer = new Player("Dealer", deck.draw(), deck.draw());
+		DealerPlayer dealer = new DealerPlayer(deck.draw(), deck.draw());
 
 		System.out.print(player.printFormatted());
 		while (player.getScore() <= 17) {
@@ -18,15 +20,15 @@ public class Main {
 			System.out.print(player.printFormatted());
 		}
 		player.stand();
-		
+
 		System.out.println("Dealer's turn:\n");
 		System.out.print(dealer.printFormatted());
-		while (dealer.getScore() < 18) {
+		while (dealer.getState() == "canHit") {
 			dealer.hit(deck.draw());
 			System.out.print(dealer.printFormatted());
 		}
 		dealer.stand();
-		
+
 	}
 
 }
