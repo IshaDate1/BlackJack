c783aa1c445acc3643448102b8bc095ede46e5af
George Troulis <georgetroulis@gmail.com>
2014-12-17 19:37:22 -0800

    Added AWT Labels for players, and all the basic GUI components

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,23 +8,104 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+
 
 public class Main
 {
     public static void main(String[] args) throws IOException 
     {
+		Font font = new Font("Arial", Font.PLAIN, 18);
+		
+		// Constants
+		int MAXPLAYERS = 4;
+		int WIDTH = 1200;
+		int HEIGHT = 900;
+  
+		// AWT elements
+		Frame BJFrame = new Frame("Java BlackJack");
+		BJFrame.setSize(WIDTH, HEIGHT);
+		BJFrame.setLayout(null);
+		BJFrame.setBackground(new Color(0, 200, 0));
+		
+		Label playerLabels[] = new Label[MAXPLAYERS];
+		Label dealerLabel = new Label();
+		dealerLabel.setText("Dealer");
+		
+		Panel controlPanel = new Panel();
+		controlPanel.setLayout(new FlowLayout());
+
+		// Input Stream
         BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
         
+		//Initializing deck and players
         Deck deck = new Deck(true);
         ArrayList<Player> players = new ArrayList<Player>(0);
         
         players.add(new Player("George", deck.draw(), deck.draw()));
         players.add(new Player("John", deck.draw(), deck.draw()));
         players.add(new Player("Evi", deck.draw(), deck.draw()));
-        
+		players.add(new Player("Fotini", deck.draw(), deck.draw()));
         DealerPlayer dealer = new DealerPlayer(deck.draw(), deck.draw());
-        
+    	
+		//DYNAMIC CONSTANTS
+		int NUMPLAYERS = players.size();
+		int PLAYER_SPACE = WIDTH/NUMPLAYERS;
+		
+		// Prepare GUI
+		BJFrame.addWindowListener(new WindowAdapter() 
+		{
+			public void windowClosing(WindowEvent windowEvent)
+			{
+				System.exit(0);
+			}        
+		});
+		
+		for(int i = 0; i < MAXPLAYERS; i ++)
+		{
+			playerLabels[i] = new Label();
+		}
+		
+		// Iterate through the number of players, and set the label alignments
+		for(int i = 0; i < NUMPLAYERS; i ++)
+		{
+			
+			playerLabels[i].setText(players.get(i).getName() + "'s Cards:");
+			playerLabels[i].setFont(font);
+			Label current = playerLabels[i];
+			BJFrame.add(current);
+			
+			FontMetrics metrics = current.getFontMetrics(font);
+			
+			int textWidth = metrics.stringWidth(current.getText());
+			int textHeight = metrics.getHeight();
+			
+			int x = ((PLAYER_SPACE-textWidth)/2) + i*PLAYER_SPACE;
+			int y = (HEIGHT * 6) / 16;
+			current.setBounds(x, y, textWidth, 20);
+			
+		}
+		dealerLabel.setText("Dealer's Cards:");
+		dealerLabel.setFont(font);
+		FontMetrics metrics = dealerLabel.getFontMetrics(font);
+		int textWidth = metrics.stringWidth(dealerLabel.getText());
+		int textHeight = metrics.getHeight();
+
+		int x = ((WIDTH-textWidth)/2);
+		int y = (HEIGHT) / 16;
+		dealerLabel.setBounds(x, y, textWidth, 20);
+		BJFrame.add(dealerLabel);
+		
+		BJFrame.add(controlPanel);
+		BJFrame.setVisible(true);  
+	  
+		////////////////////////////////////////////////////////////////////////
+	    //////////    Main Game loop 
+		////////////////////////////////////////////////////////////////////////
         for (Player player : players)
         {
             System.out.print(player.printFormatted());
