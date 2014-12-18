ff968a4e7c0bb70f016611155265c11d8db2fd1a
George Troulis <georgetroulis@gmail.com>
2014-12-17 16:10:56 -0800

    removed all swing files, will probably end up being an awt application

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,25 +8,11 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 public class Main
 {
-
     public static void main(String[] args) throws IOException 
     {
-        SwingUtilities.invokeLater(new Runnable() 
-        {
-            public void run()
-            {
-                JFrame frame = new JFrame("BlackJack");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
-                frame.setSize(500, 400);
-            }
-        });
-        
         BufferedReader br = 
             new BufferedReader(new InputStreamReader(System.in));
         
